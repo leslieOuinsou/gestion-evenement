4338724405c943c902e0af14780e273432b9c904
Leslie OUINSOU <ouinsoul5@gmail.com>
2025-05-13 06:33:03 +0200

    Réintégration de package.json et package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React, { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Home from "./Pages/Home";
 import Loading from "./Loading"; // Importe le composant Loading
 import Login from "./components/Login";
 import Logout from "./components/Logout"; // Importe la page de Logout
 import Signup from "./components/Signup"; // Importe la page d'inscription
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Profile from "./components/Profile"; // Importe la page Profile
 import Events from "./Pages/Events"; // Importe la page Events
 import Notifications from "./Pages/Notifications"; // Importe la page Notifications
@@ -23,6 +23,7 @@ const App = () => {
       document.body.removeChild(script);
     };
   }, []);
+
   // Définir un utilisateur par défaut (ex. un objet utilisateur pour tester)
   const [user, setUser] = useState({
     name: "John Doe",
@@ -30,15 +31,15 @@ const App = () => {
     photo: "profile.jpg",
   });
 
-  const location = useLocation();  // Utilise useLocation pour accéder à l'URL actuelle
+  const location = useLocation(); // Utilise useLocation pour accéder à l'URL actuelle
 
   // Condition pour afficher ou non la navbar en fonction de l'URL
   const shouldShowNavbar =
     location.pathname !== "/notifications" &&
     location.pathname !== "/login" &&
     location.pathname !== "/logout" &&
-    location.pathname !== "/signup"&&
-    location.pathname !== "/settings"&&
+    location.pathname !== "/signup" &&
+    location.pathname !== "/settings" &&
     location.pathname !== "/";
 
   return (
@@ -46,7 +47,7 @@ const App = () => {
       {shouldShowNavbar && <Navbar />} {/* Affiche la Navbar uniquement si la condition est vraie */}
       <Routes>
         <Route path="/" element={<Loading />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/events" element={<Events />} />
